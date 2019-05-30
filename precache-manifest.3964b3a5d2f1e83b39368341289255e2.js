df1b37ce62013215f7503382ad455664a7e476aa
wjzz <wjedynak@gmail.com>
2019-05-30 23:29:29 +0200

    Updates

---
+++ precache-manifest.3964b3a5d2f1e83b39368341289255e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c2a0fe9104e3057e3afe3bb7ebbf30",
+    "url": "/sudoku-react/index.html"
+  },
+  {
+    "revision": "265f46795279666ff16e",
+    "url": "/sudoku-react/static/css/main.202bb072.chunk.css"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/sudoku-react/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "265f46795279666ff16e",
+    "url": "/sudoku-react/static/js/main.055ee924.chunk.js"
+  },
+  {
+    "revision": "01b864d0c040dc6f0b6c",
+    "url": "/sudoku-react/static/js/runtime~main.e1ff4c15.js"
+  }
+]);
